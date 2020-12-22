88c8b4c23e515ccf912eb39be68c1321b785aae2
nash <nash@iffy.me>
2020-12-22 23:58:10 +0800

    yaml & json schemas

--- config.lua/lsp.lua
+++ config.lua/lsp.lua
@@ -16,8 +16,68 @@ local user_cfg = {
     tsserver = {},
     -- golang: https://github.com/golang/tools/tree/master/gopls
     gopls = {},
-    yamlls = {},
-    jsonls = {},
+    yamlls = {
+      settings = {
+        yaml = {
+          schemas = {
+            ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*.{yml,yaml}',
+            ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+            ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
+            ['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
+            ['http://json.schemastore.org/stylelintrc'] = '.stylelintrc.{yml,yaml}',
+            ['http://json.schemastore.org/circleciconfig'] = '.circleci/**/*.{yml,yaml}'
+          }
+        }
+      }
+    },
+    jsonls = {
+      settings = {
+        json = {
+          schemas = {
+            {
+              description = 'TypeScript compiler configuration file',
+              fileMatch = {'tsconfig.json', 'tsconfig.*.json'},
+              url = 'http://json.schemastore.org/tsconfig'
+            },
+            {
+              description = 'Lerna config',
+              fileMatch = {'lerna.json'},
+              url = 'http://json.schemastore.org/lerna'
+            },
+            {
+              description = 'Babel configuration',
+              fileMatch = {'.babelrc.json', '.babelrc', 'babel.config.json'},
+              url = 'http://json.schemastore.org/lerna'
+            },
+            {
+              description = 'ESLint config',
+              fileMatch = {'.eslintrc.json', '.eslintrc'},
+              url = 'http://json.schemastore.org/eslintrc'
+            },
+            {
+              description = 'Bucklescript config',
+              fileMatch = {'bsconfig.json'},
+              url = 'https://bucklescript.github.io/bucklescript/docson/build-schema.json'
+            },
+            {
+              description = 'Prettier config',
+              fileMatch = {'.prettierrc', '.prettierrc.json', 'prettier.config.json'},
+              url = 'http://json.schemastore.org/prettierrc'
+            },
+            {
+              description = 'Vercel Now config',
+              fileMatch = {'now.json'},
+              url = 'http://json.schemastore.org/now'
+            },
+            {
+              description = 'Stylelint config',
+              fileMatch = {'.stylelintrc', '.stylelintrc.json', 'stylelint.config.json'},
+              url = 'http://json.schemastore.org/stylelintrc'
+            },
+          }
+        },
+      }
+    },
     sqlls = {},
     vuels = {},
     sumneko_lua = {},
